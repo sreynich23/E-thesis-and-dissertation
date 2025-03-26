9870e210ca2af49a29632bef6e3d5b120a9e9cde
Sreynich <longsreynich231@gmail.com>
2025-03-26 16:10:44 +0700

    update

--- database/migrations/2025_01_28_085632_create_books_table.php
+++ database/migrations/2025_01_28_085632_create_books_table.php
@@ -12,17 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('books', function (Blueprint $table) {
-            $table->id();  // Auto-incrementing primary key
-            $table->unsignedBigInteger('id_majors');  // Foreign key for majors
-            $table->unsignedBigInteger('id_generation');  // Foreign key for generation
-            $table->string('title');  // File path for the cover image
-            $table->string('cover')->nullable();  // File path for the cover image
-            $table->string('path_file');  // File path for the book (PDF, etc.)
-            $table->timestamps();  // created_at and updated_at timestamps
+            $table->id();
+            $table->unsignedBigInteger('id_majors');
+            $table->string('generation')->nullable();
+            $table->year('year')->nullable();
+            $table->string('title');
+            $table->string('path_file');
+            $table->timestamps();
 
-            // Foreign key constraints (assuming you have 'majors' and 'generations' tables)
             $table->foreign('id_majors')->references('id')->on('majors')->onDelete('cascade');
-            $table->foreign('id_generation')->references('id')->on('generations')->onDelete('cascade');
         });
     }
 
