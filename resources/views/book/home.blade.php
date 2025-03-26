369c1bdf6413edbd54d40734181cb7643af0333b
Sreynich <longsreynich231@gmail.com>
2025-03-26 15:05:42 +0700

    update

--- resources/views/book/home.blade.php
+++ resources/views/book/home.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NUBB Finish The Dissertation</title>
+    <link rel="icon" type="image/png" href="{{ asset('storage/logo/logo.jpg') }}">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
--- resources/views/book/show.blade.php
+++ resources/views/book/show.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-thesis and dissertation</title>
+    <link rel="icon" type="image/png" href="{{ asset('storage/logo/logo.jpg') }}">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
