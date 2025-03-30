8e73f53498443c6c643e81f0844fa269e17560be
Sreynich <longsreynich231@gmail.com>
2025-03-31 00:14:50 +0700

    update

--- resources/views/book/home.blade.php
+++ resources/views/book/home.blade.php
@@ -24,6 +24,7 @@
             style="background-image: url('{{ asset('storage/logo/navbar.png') }}'); background-size: cover; background-position: center; background-repeat: no-repeat;">
             <div class="space-x-4 flex items-center absolute top-1 lg:top-4 right-4 lg:right-12 transform">
                 @if (Auth::check())
+                    @if (Auth::guard('student')->check())
                     <button @click="showProfile = !showProfile">
                         <i class="fas fa-user text-white"></i>
                     </button>
@@ -33,6 +34,16 @@
                             <i class="fas fa-sign-out-alt"></i>
                         </button>
                     </form>
+                    @else
+                        <a href="{{ route('books') }}"
+                            class="bg-green-500 hover:bg-green-600 text-white p-2 rounded-lg">Admin Dashboard</a>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <button class="bg-red-500 hover:bg-red-600 text-white p-2 rounded-lg">
+                                    <i class="fas fa-sign-out-alt"></i>
+                                </button>
+                            </form>
+                    @endif
                 @else
                     <a href="{{ route('login') }}"
                         class="lg:bg-green-500 hover:bg-green-600 text-white lg:p-2 lg:rounded-lg lg:text-base text-xs">Login</a>
@@ -42,26 +53,19 @@
     </nav>
     <div class="space-y-4 w-full md:w-auto">
         <div class="flex items-center lg:space-x-4 space-x-2 justify-self-center">
-
             <!-- Generation Dropdown -->
-            <div class="relative" x-data="{ open: false, Generation: '' }">
-                <button @click="open = !open"
-                    class="nav-link flex items-center text-xs md:text-sm lg:text-base bg-red-800 text-white lg:py-2 lg:px-10 lg:rounded-md p-1">
-                    <span x-text="Generation || 'Select Generation'"></span>
+            <div class="relative" x-data="{ open: false }">
+                <button @click="open = !open" class="nav-link flex items-center text-xs md:text-sm lg:text-base bg-red-800 text-white lg:py-2 lg:px-10 lg:rounded-md p-1">
+                    <span x-text="Generation"></span>
                     <svg class="ml-1 h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd"
-                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                            clip-rule="evenodd" />
+                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                     </svg>
-                    <!-- Clear Button -->
-                    <span x-show="Generation" @click.stop="Generation = ''" class="ml-2 text-gray-300 hover:text-gray-500">✕</span>
+                    <span x-show="Generation" @click.stop="Generation = 'Generation'" class="ml-2 text-gray-300 hover:text-gray-500">✕</span>
                 </button>
-                <div x-show="open" @click.away="open = false"
-                    class="absolute right-0 lg:mt-2 lg:w-48 bg-white lg:rounded-md shadow-lg py-1 z-10 overflow-y-auto min-h-full max-h-min h-40">
+                <div x-show="open" @click.away="open = false" class="absolute right-0 lg:mt-2 lg:w-48 bg-white lg:rounded-md shadow-lg py-1 z-10 overflow-y-auto min-h-full max-h-min h-40">
                     <div class="space-y-1">
                         @foreach ($generations as $generation => $value)
-                            <a @click="Generation = '{{ $generation }}'; open = false"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 lg:rounded-md">
+                            <a @click="Generation = '{{ $generation }}'; open = false" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 lg:rounded-md">
                                 {{ $generation }}
                             </a>
                         @endforeach
@@ -70,24 +74,18 @@
             </div>
 
             <!-- Major Dropdown -->
-            <div class="relative" x-data="{ open: false, Major: '' }">
-                <button @click="open = !open"
-                    class="nav-link flex items-center text-xs md:text-sm lg:text-base bg-red-800 text-white lg:py-2 lg:px-10 lg:rounded-md p-1">
-                    <span x-text="Major || 'Select Major'"></span>
+            <div class="relative" x-data="{ open: false }">
+                <button @click="open = !open" class="nav-link flex items-center text-xs md:text-sm lg:text-base bg-red-800 text-white lg:py-2 lg:px-10 lg:rounded-md p-1">
+                    <span x-text="Major"></span>
                     <svg class="ml-1 h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd"
-                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                            clip-rule="evenodd" />
+                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                     </svg>
-                    <!-- Clear Button -->
-                    <span x-show="Major" @click.stop="Major = ''" class="ml-2 text-gray-300 hover:text-gray-500">✕</span>
+                    <span x-show="Major" @click.stop="Major = 'Major'" class="ml-2 text-gray-300 hover:text-gray-500">✕</span>
                 </button>
-                <div x-show="open" @click.away="open = false"
-                    class="absolute lg:right-0 lg:mt-2 lg:w-80 bg-white lg:rounded-md shadow-lg py-1 z-10 overflow-y-auto min-h-full max-h-min w-40 h-40 lg:h-80">
+                <div x-show="open" @click.away="open = false" class="absolute right-0 lg:mt-2 lg:w-72 bg-white lg:rounded-md shadow-lg py-1 z-10 overflow-y-auto min-h-full max-h-min h-40">
                     <div class="space-y-1">
                         @foreach ($majors as $major)
-                            <a @click="Major = '{{ $major->major_name }}'; open = false"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 lg:rounded-md">
+                            <a @click="Major = '{{ $major->major_name }}'; open = false" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 lg:rounded-md">
                                 {{ $major->major_name }}-{{ $major->khmer_name }}
                             </a>
                         @endforeach
@@ -96,37 +94,33 @@
             </div>
 
             <!-- Year Dropdown -->
-            <div class="relative" x-data="{ open: false, Year: '' }">
-                <button @click="open = !open"
-                    class="nav-link flex items-center text-xs md:text-sm lg:text-base bg-red-800 text-white lg:py-2 lg:px-10 lg:rounded-md p-1">
-                    <span x-text="Year || 'Select Year'"></span>
+            <div class="relative" x-data="{ open: false }">
+                <button @click="open = !open" class="nav-link flex items-center text-xs md:text-sm lg:text-base bg-red-800 text-white lg:py-2 lg:px-10 lg:rounded-md p-1">
+                    <span x-text="Year"></span>
                     <svg class="ml-1 h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd"
-                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                            clip-rule="evenodd" />
+                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                     </svg>
-                    <!-- Clear Button -->
-                    <span x-show="Year" @click.stop="Year = ''" class="ml-2 text-gray-300 hover:text-gray-500">✕</span>
+                    <span x-show="Year" @click.stop="Year = 'Year'" class="ml-2 text-gray-300 hover:text-gray-500">✕</span>
                 </button>
-                <div x-show="open" @click.away="open = false"
-                    class="absolute right-0 lg:mt-2 lg:w-48 bg-white lg:rounded-md shadow-lg py-1 z-10 overflow-y-auto h-40">
+                <div x-show="open" @click.away="open = false" class="absolute right-0 lg:mt-2 lg:w-48 bg-white lg:rounded-md shadow-lg py-1 z-10 overflow-y-auto h-40">
                     <div class="space-y-1">
                         @foreach ($years as $year => $value)
-                            <a @click="Year = '{{ $year }}'; open = false"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 lg:rounded-md">
+                            <a @click="Year = '{{ $year }}'; open = false" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 lg:rounded-md">
                                 {{ $year }}
                             </a>
                         @endforeach
                     </div>
                 </div>
             </div>
-
-            <!-- Search Input with Clear Button -->
+            <!-- Search Input -->
             <div class="relative">
                 <input type="text" x-model="search" placeholder="Search..."
-                    class="px-4 py-2 border-red-800 border-2 lg:rounded-md text-black w-full md:w-auto lg:w-80 pr-10">
+                       class="px-4 py-2 border-red-800 border-2 lg:rounded-md text-black w-full md:w-auto lg:w-80 pr-10">
+                <!-- Clear button (X) -->
                 <button x-show="search" @click="search = ''"
-                    class="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700">✕</button>
+                        class="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700">
+                    ✕
+                </button>
             </div>
 
         </div>
--- resources/views/book/show.blade.php
+++ resources/views/book/show.blade.php
@@ -17,102 +17,6 @@
 <body x-data="{ Major: 'Major', Generation: 'Generation', Year: 'Year', search: '', showProfile: false }">
     <nav class="border-b text-black sticky">
         <img src="{{ asset('storage/logo/navbar.jpg') }}" class="w-full">
-        <div class="flex justify-between items-center px-6 pt-3 bg-[url('storage/logo/navbar2.png')] bg-cover bg-center">
-            <!-- Left Side for Generation, Major, Year Dropdowns and Search Input -->
-            <div class="space-y-4 w-full md:w-auto">
-                <div class="flex items-center lg:space-x-4 space-x-2 justify-self-center">
-                    <!-- Generation Dropdown -->
-                    <div class="relative" x-data="{ open: false }">
-                        <button @click="open = !open" class="nav-link flex items-center text-xs md:text-sm lg:text-base bg-red-800 text-white lg:py-2 lg:px-10 lg:rounded-md p-1">
-                            <span x-text="Generation"></span>
-                            <svg class="ml-1 h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                            </svg>
-                        </button>
-                        <div x-show="open" @click.away="open = false" class="absolute right-0 lg:mt-2 lg:w-48 bg-white lg:rounded-md shadow-lg py-1 z-10 overflow-y-auto min-h-full max-h-min h-40">
-                            <div class="space-y-1">
-                                @foreach ($generations as $generation => $value)
-                                    <a @click="Generation = '{{ $generation }}'; open = false" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 lg:rounded-md">
-                                        {{ $generation }}
-                                    </a>
-                                @endforeach
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Major Dropdown -->
-                    <div class="relative" x-data="{ open: false }">
-                        <button @click="open = !open" class="nav-link flex items-center text-xs md:text-sm lg:text-base bg-red-800 text-white lg:py-2 lg:px-10 lg:rounded-md p-1">
-                            <span x-text="Major"></span>
-                            <svg class="ml-1 h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                            </svg>
-                        </button>
-                        <div x-show="open" @click.away="open = false" class="absolute right-0 lg:mt-2 lg:w-48 bg-white lg:rounded-md shadow-lg py-1 z-10 overflow-y-auto min-h-full max-h-min h-40">
-                            <div class="space-y-1">
-                                @foreach ($majors as $major)
-                                    <a @click="Major = '{{ $major->major_name }}'; open = false" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 lg:rounded-md">
-                                        {{ $major->major_name }}-{{ $major->khmer_name }}
-                                    </a>
-                                @endforeach
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Year Dropdown -->
-                    <div class="relative" x-data="{ open: false }">
-                        <button @click="open = !open" class="nav-link flex items-center text-xs md:text-sm lg:text-base bg-red-800 text-white lg:py-2 lg:px-10 lg:rounded-md p-1">
-                            <span x-text="Year"></span>
-                            <svg class="ml-1 h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                            </svg>
-                        </button>
-                        <div x-show="open" @click.away="open = false" class="absolute right-0 lg:mt-2 lg:w-48 bg-white lg:rounded-md shadow-lg py-1 z-10 overflow-y-auto h-40">
-                            <div class="space-y-1">
-                                @foreach ($years as $year => $value)
-                                    <a @click="Year = '{{ $year }}'; open = false" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 lg:rounded-md">
-                                        {{ $year }}
-                                    </a>
-                                @endforeach
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Search Input -->
-                    <input type="text" x-model="search" placeholder="Search..." class="px-4 py-2 border-red-800 border-2 lg:rounded-md text-black w-full md:w-auto lg:w-80">
-                </div>
-            </div>
-
-            <!-- Mobile Hamburger Menu Button -->
-            <div class="block md:hidden">
-                <button @click="mobileMenu = !mobileMenu" class="text-white p-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
-                    </svg>
-                </button>
-            </div>
-        </div>
-
-        <!-- Mobile Dropdown Menu -->
-        <div x-show="mobileMenu" class="block md:hidden bg-white shadow-md py-4">
-            <div class="space-y-4">
-                <div class="px-6">
-                    <div class="relative">
-                        <button @click="open = !open" class="flex items-center text-xs md:text-sm lg:text-base bg-white lg:py-2 lg:px-3 rounded-md">
-                            <span x-text="Generation"></span>
-                        </button>
-                        <div x-show="open" @click.away="open = false" class="absolute right-0 lg:mt-2 lg:w-48 bg-white rounded-md shadow-lg py-1 z-10 overflow-y-auto h-40">
-                            <div class="space-y-1">
-                                @foreach ($generations as $generation => $value)
-                                    <a @click="Generation = '{{ $generation }}'; open = false" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded-md">
-                                        {{ $generation }}
-                                    </a>
-                                @endforeach
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </nav>
     <button onclick="window.location.href='{{ url('/') }}'" class="text-blue-500 p-5">
         ⇽ Back Home
@@ -139,7 +43,7 @@
             </div>
 
             @if ($book->path_file && Storage::disk('public')->exists($book->path_file))
-            <embed src="{{ asset('storage/' . $book->path_file) }}#toolbar=0&scrollbar=0&zoom=FitV&page=1" type="application/pdf"
+            <embed src="{{ asset('storage/' . $book->path_file) }}#toolbar=0&scrollbar=0&view=FitV&page=1" type="application/pdf"
                 class="h-96 lg:w-full lg:h-full border-none justify-self-center bg-white" />
             @else
                 <p class="text-red-500">The requested resource was not found on this server.</p>
